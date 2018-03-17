2ccd71a9ed70d92a6362fcae3260a3f1d4c790d7
Alberto Rocha <betogrun@gmail.com>
2018-03-17 01:02:41 -0300

    Setup CORS and Rack attack

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module RememberTheBills
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::Attack
   end
 end
