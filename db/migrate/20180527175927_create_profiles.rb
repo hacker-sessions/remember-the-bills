bcd04a5420070b001ed4b73868db51d877bc7467
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2018-05-27 15:29:16 -0300

    Create profile model

---
+++ db/migrate/20180527175927_create_profiles.rb
@@ -0,0 +1,11 @@
+class CreateProfiles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :profiles do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.integer :kind
+
+      t.timestamps
+    end
+  end
+end
